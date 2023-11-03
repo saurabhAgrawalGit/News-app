c559e84efbfc894209ece2eb003e198543de6da8
saurabhAgrawalGit <saurabhagrawal622@gmail.com>
2023-11-03 22:05:40 +0530

    add tab layout with working

--- app/src/main/java/com/example/newsapp/MainActivity.java
+++ app/src/main/java/com/example/newsapp/MainActivity.java
@@ -27,5 +27,32 @@ public class MainActivity extends AppCompatActivity {
         healthin=findViewById(R.id.health);
         sportin=findViewById(R.id.sports);
         viewPager=findViewById(R.id.frag);
+
+        pagerAdapterOBJ =new pagerAdapter(getSupportFragmentManager(),6);
+
+        viewPager.setAdapter(pagerAdapterOBJ);
+
+        tabLayoutin.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+
+                if(tab.getPosition()==0||tab.getPosition()==1||tab.getPosition()==2||tab.getPosition()==3||tab.getPosition()==4||tab.getPosition()==5)
+                {
+                    pagerAdapterOBJ.notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayoutin));
     }
 }
